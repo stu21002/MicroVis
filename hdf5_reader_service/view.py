98513bccab7bd1ebb544f4ed94f1a38ddcb16044
Stuart Heath <stuartheath02@gmail.com>
2024-06-06 13:58:35 +0200

    python file to help viewing h5 files

--- hdf5_reader_service/view.py
+++ hdf5_reader_service/view.py
@@ -10,6 +10,9 @@ def read_hdf5_detailed(file_path):
                 data[key] = recursive_extract(item)
             elif isinstance(item, h5py.Dataset):
                 data[key] = item[()]
+                if key == "DATA":
+                    for val in item:
+                        print(val)
         return data
 
     with h5py.File(file_path, 'r') as file:
@@ -18,20 +21,20 @@ def read_hdf5_detailed(file_path):
     return structure
 
 # Specify the path to your HDF5 file
-file_path = 'path_to_your_file/example.hdf5'
+file_path = './files/example.hdf5'
 
 # Read the HDF5 file and get the detailed data
 hdf5_detailed_data = read_hdf5_detailed(file_path)
 
-# Function to pretty-print the nested dictionary
-def print_nested_dict(d, indent=0):
-    for key, value in d.items():
-        print(' ' * indent + str(key))
-        if isinstance(value, dict):
-            print_nested_dict(value, indent + 4)
-        else:
-            print(' ' * (indent + 4) + str(value))
+# # Function to pretty-print the nested dictionary
+# def print_nested_dict(d, indent=0):
+#     for key, value in d.items():
+#         print(' ' * indent + str(key))
+#         if isinstance(value, dict):
+#             print_nested_dict(value, indent + 4)
+#         else:
+#             print(' ' * (indent + 4) + str(value))
 
-# Print the structure and content of the HDF5 file
-print_nested_dict(hdf5_detailed_data)
+# # Print the structure and content of the HDF5 file
+# print_nested_dict(hdf5_detailed_data)
 
